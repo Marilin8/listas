8cc6e4658e7629d5f7869eaff1a93c5e5aff496b
Marilin8 <marilinyaque4061@gmail.com>
2024-02-22 22:37:03 -0600

    listas_simplemente_enlazadas
    listas

---
+++ src/listas_simplemente_enlazadas/Nodo.java
@@ -0,0 +1,16 @@
+package listas_simplemente_enlazadas;
+
+public class Nodo {
+	
+int dato;
+	
+	Nodo siguiente;
+	
+	
+	public Nodo(int dato) {
+		this.dato = dato;
+		this.siguiente = null;
+	}
+	
+
+}
